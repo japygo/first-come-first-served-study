79440d7b710b911598f9d3ec3d690fb98183e3b7
japygo <hyunnjinn@gmail.com>
2024-09-18 16:47:53 +0900

    Redis를 활용하여 문제 해결하기

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.api.domain.Coupon;
+import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 
 @Service
@@ -11,13 +12,16 @@ public class ApplyService {
 
     private final CouponRepository couponRepository;
 
-    public ApplyService(CouponRepository couponRepository) {
+    private final CouponCountRepository couponCountRepository;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
         this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
     }
 
     @Transactional
     public void apply(Long userId) {
-        long count = couponRepository.count();
+        Long count = couponCountRepository.increment();
 
         if (count > 100) {
             return;
